3a3a42194459df0a07f0c6ea88d73e980eb5965f
niubilityxiesong <563527588@qq.com>
2018-07-17 03:10:18 +0800

    tempsave section_2

--- practices/section_2/practice_1/practice.js
+++ practices/section_2/practice_1/practice.js
@@ -1,5 +1,26 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  let countNum = 1;
+  let result = [];
+
+  for (let i = 0; i < collection.length - 1; i++) {
+    if(collection[i] == collection[i + 1]){
+      countNum++;
+    }
+    else {
+      var tempMap = {};
+      tempMap.key = collection[i];
+      tempMap.count = countNum;
+      result.push(tempMap);
+      countNum = 1;
+    }
+  }
+
+  var tempMap = {};
+  tempMap.key = collection[collection.length - 1];
+  tempMap.count = countNum;
+  result.push(tempMap);
+  return result;
 }
 
 module.exports = count_same_elements;
--- practices/section_2/practice_2/practice.js
+++ practices/section_2/practice_2/practice.js
@@ -1,5 +1,26 @@
 function count_same_elements(collection) {
   //在这里写入代码
+  let countNum = 1;
+  let result = [];
+
+  for (let i = 0; i < collection.length - 1; i++) {
+    if(collection[i] == collection[i + 1]){
+      countNum++;
+    }
+    else {
+      var tempMap = {};
+      tempMap.key = collection[i];
+      tempMap.count = countNum;
+      result.push(tempMap);
+      countNum = 1;
+    }
+  }
+  var tempMap = {};
+  tempMap.key = collection[collection.length - 1][0];
+  tempMap.count = parseInt(collection[collection.length - 1][2]);
+  result.push(tempMap);
+  console.log(result);
+  return result;
 }
 
 module.exports = count_same_elements;
