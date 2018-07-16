795138479de034e905f3b8a2e7fa2ea44f9540a0
niubilityxiesong <563527588@qq.com>
2018-07-16 23:24:40 +0800

    finish flaten

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,13 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+
+  let result = [];
+  for (let i = 0; i < collection.length; i++) {
+    result = result.concat(collection[i]);
+  }
+
+  return result;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,19 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  let result = [];
+  let array = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    array = array.concat(collection[i]);
+  }
+
+  array.map(elm => {
+    if (result.indexOf(elm) == -1){
+      result.push(elm);
+    }
+  });
+  return result;
 }
 
 module.exports = double_to_one;
