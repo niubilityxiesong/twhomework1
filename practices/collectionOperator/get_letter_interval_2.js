b74f585313a92bb9a10b439dd14faeded3d08a97
niubilityxiesong <563527588@qq.com>
2018-07-16 20:48:44 +0800

    finish collectionOperator

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -3,14 +3,35 @@
 function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
 
-  let max = number_a > number_b ? number_a : number_b;
-  let min = number_a < number_b ? number_a : number_b;
-
+  let judge_rise = number_a > number_b ? true : false;
+  let max = judge_rise ? number_a : number_b;
+  let min = judge_rise ? number_b : number_a;
   let result = [];
+  const LIST_ARRAT = 'abcdefghijklmnopqrstuvwxyz';
+  const NUMBER_OF_ARRAY = 26;
 
+  for (let i = min; i <= max; i++) {
+    if (i <= NUMBER_OF_ARRAY){
+      result.push(LIST_ARRAT[i - 1]);
+    }
+    else {
+      let temp_array = "";
+      let remainder = i % NUMBER_OF_ARRAY;
+      let merchant = Math.floor(i / NUMBER_OF_ARRAY);
 
+      if(remainder == 0){
+        remainder = NUMBER_OF_ARRAY;
+        merchant--;
+      }
+      temp_array = LIST_ARRAT[merchant - 1] + LIST_ARRAT[remainder - 1];
+      result.push(temp_array);
+    }
+  }
 
-  return result;
+  if(!judge_rise)
+    return result;
+  else
+    return result.reverse();
 }
 
 module.exports = get_letter_interval_2;
--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,11 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  let result = [];
+
+  result = collection_a.filter(elm => {return result.indexOf(elm) === -1});
+  result = result.concat(collection_b.filter(elm => {return result.indexOf(elm) === -1}));
+  return result;
 }
 
 module.exports = get_union;
